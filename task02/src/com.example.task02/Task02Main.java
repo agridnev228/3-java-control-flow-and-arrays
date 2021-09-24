75241e360e85f528c0d5d8cb32ba047f660d164b
agridnev228 <agridnev228@gmail.com>
2021-09-24 15:27:10 +0500

    task02

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -4,12 +4,33 @@ public class Task02Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        System.out.println(getSeason(12));
-         */
+
+        System.out.println(getSeason(11));
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+       //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String str="";
+        switch(monthNumber) {
+            case 1:
+            case 2:
+            case 12: str="зима";
+            break;
+            case 3:
+            case 4:
+            case 5: str="весна";
+            break;
+            case 6:
+            case 7:
+            case 8: str="лето";
+            break;
+            case 9:
+            case 10:
+            case 11: str="осень";
+            break;
+            default: break;
+        }
+        return str;
     }
 }
