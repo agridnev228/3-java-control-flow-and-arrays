c38d97ec3e274281a506ff84c2280446dda99a72
agridnev228 <agridnev228@gmail.com>
2021-09-25 20:34:39 +0500

    task06

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -4,14 +4,20 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getMax(1, 2, 3, 4));
-         */
+
     }
 
     static int getMax(int a, int b, int c, int d) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        if(a<b)
+            a=b;
+        if(a<c)
+            a=c;
+        if(a<d)
+            a=d;
+        return a;
     }
 
 }
